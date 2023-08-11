c19f56c6a12c93ee28a87b5c91fdca82ae5813a9
Thor <djcat211@gmail.com>
2023-08-11 09:28:23 +0800

    Experimental Commit Java Script

--- code.js
+++ code.js
@@ -1,1 +1,10 @@
 // im creating a new game, this code is obsolete but im trying my best to do basic stuff
+let myVar = 1;
+
+//setInterval (myMethod, 1000);
+//setTimeout (myMethod, 6)
+function myMethod ()
+{
+myVar = ++myVar;
+console.log(myVar)
+}
