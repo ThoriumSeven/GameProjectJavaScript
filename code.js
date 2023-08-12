744c775d8c607379ff646dd8142bde4123089265
Thor <djcat211@gmail.com>
2023-08-12 10:22:50 +0800

    Adding comments

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 // im creating a new game, this code is obsolete but im trying my best to do basic stuff
 let myVar = 1;
-
+// uncomment the code below for function loop. 
 //setInterval (myMethod, 1000);
 //setTimeout (myMethod, 6)
 function myMethod ()
