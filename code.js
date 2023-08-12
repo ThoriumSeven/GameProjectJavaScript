0492d9eef662aa28557ba83371333954a944b329
Thor <djcat211@gmail.com>
2023-08-12 13:17:18 +0800

    Experimenting with Concatenation in JavaScript

--- code.js
+++ code.js
@@ -3,8 +3,25 @@ let myVar = 1;
 // uncomment the code below for function loop. 
 //setInterval (myMethod, 1000);
 //setTimeout (myMethod, 6)
+/*
+
 function myMethod ()
 {
 myVar = ++myVar;
 console.log(myVar)
+}
+
+*/
+
+//making new stuffs
+let myNum = 0;
+setInterval (myFunction, 1000);
+function myFunction ()
+{
+    let myStr = "Hello, ";
+    const myPlace = "World >";
+    myStr += myPlace;
+    myNum = ++myNum;
+    myStr += myNum
+    console.log(myStr);
 }
